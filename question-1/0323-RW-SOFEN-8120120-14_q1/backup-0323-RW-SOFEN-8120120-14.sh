dd4ae410c0c5c4541f33e0b70e15d26439f3cc51
Obolo Emmanuel <e.obolo@alustudent.com>
2023-06-23 23:24:02 -0700

    made new changes and git pull changes

--- question-1/0323-RW-SOFEN-8120120-14_q1/backup-0323-RW-SOFEN-8120120-14.sh
+++ question-1/0323-RW-SOFEN-8120120-14_q1/backup-0323-RW-SOFEN-8120120-14.sh
@@ -4,7 +4,6 @@
 # Remote server details
 HOST="a0dfc11ba139.a982e858.alu-cod.online"
 USERNAME="a0dfc11ba139"
-PASSWORD="899c79fc532cd0591af1"
 
 # Directory to be backed up
 SOURCE_DIR="0323-RW-SOFEN-8120120-14_q1"
@@ -18,8 +17,8 @@ BACKUP_FILE="${SOURCE_DIR}.tar.gz"
 # Compress the source directory into a tarball
 tar -czf "${BACKUP_FILE}" "${SOURCE_DIR}"
 
-# Upload the backup file to the remote server using SFTP
-sftppass -p "${PASSWORD}" sftp "${USERNAME}@${HOST}:${REMOTE_DIR}" <<< "put ${BACKUP_FILE}"
+# Upload the backup file to the remote server using SCP
+scp "${BACKUP_FILE}" "${USERNAME}@${HOST}:${REMOTE_DIR}"
 
 # Clean up the local backup file
 rm "${BACKUP_FILE}"
